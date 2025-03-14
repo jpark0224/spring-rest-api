7647f9f18afa91777e73149997870249e18ce245
Julie Park <julie.park01@bbc.co.uk>
2025-03-14 15:41:56 +0000

    first commit

---
+++ src/main/java/exercisetracker/Exercise.java
@@ -0,0 +1,68 @@
+package exercisetracker;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+public class Exercise {
+
+    private @Id @GeneratedValue Long id;
+
+    private String name;
+    private String primaryMuscleGroup;
+
+    Exercise(String name, String primaryMuscleGroup) {
+        this.name = name;
+        this.primaryMuscleGroup = primaryMuscleGroup;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPrimaryMuscleGroup() {
+        return this.primaryMuscleGroup;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrimaryMuscleGroup(String primaryMuscleGroup) {
+        this.primaryMuscleGroup = primaryMuscleGroup;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Exercise exercise = (Exercise) o;
+        return Objects.equals(id, exercise.id) && Objects.equals(name, exercise.name) && Objects.equals(primaryMuscleGroup, exercise.primaryMuscleGroup);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, primaryMuscleGroup);
+    }
+
+    @Override
+    public String toString() {
+        return "Exercise{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", primaryMuscleGroup='" + primaryMuscleGroup + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/exercisetracker/ExercisetrackerApplication.java
@@ -0,0 +1,13 @@
+package exercisetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercisetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercisetrackerApplication.class, args);
+	}
+
+}
