becea196896262102cd1aa2f1503f152e7838986
Julie Park <julie.park01@bbc.co.uk>
2025-03-17 17:31:39 +0000

    Add CRUD to exercise

--- src/main/java/exercisetracker/ExerciseController.java
+++ src/main/java/exercisetracker/ExerciseController.java
@@ -5,8 +5,10 @@ import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
+import org.apache.coyote.Response;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ExerciseController {
 
     private final ExerciseRepository exerciseRepository;
@@ -49,5 +52,44 @@ public class ExerciseController {
                 .orElseThrow(() -> new ExerciseNotFoundException(id));
 
         return assembler.toModel(order);
+    }
+
+    @PostMapping("/exercises")
+    ResponseEntity<EntityModel<Exercise>> newExercise(@RequestBody Exercise exercise) {
+
+        Exercise newExercise = exerciseRepository.save(exercise);
+
+        return ResponseEntity //
+                .created(linkTo(methodOn(ExerciseController.class).one(newExercise.getId())).toUri()) //
+                .body(assembler.toModel(newExercise));
+    }
+
+    @PutMapping("/exercises/{id}")
+    ResponseEntity<?> replaceExercise(@RequestBody Exercise newExercise, @PathVariable Long id) {
+
+        Exercise updatedExercise = exerciseRepository.findById(id)
+                .map(exercise -> {
+                    exercise.setName(newExercise.getName());
+                    exercise.setPrimaryMuscleGroup(newExercise.getPrimaryMuscleGroup());
+                    return exerciseRepository.save(exercise);
+                })
+                .orElseGet(() -> {
+                    return exerciseRepository.save(newExercise);
+                });
+
+        EntityModel<Exercise> entityModel = assembler.toModel(updatedExercise);
+
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
+
+    }
+
+    @DeleteMapping("/exercises/{id}")
+    ResponseEntity<?> deleteExercise(@PathVariable Long id) {
+
+        exerciseRepository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/exercisetracker/StatusController.java
@@ -0,0 +1,33 @@
+package exercisetracker;
+
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+class Status {
+    private int statusCode;
+    private String message;
+
+    public Status(int statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
+
+@RestController
+public class StatusController {
+
+    @GetMapping("/status")
+    public ResponseEntity<Status> getStatus() {
+        return ResponseEntity.ok(new Status(200, "OK"));
+    }
+}
